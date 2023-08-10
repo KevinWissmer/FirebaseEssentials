13936d1c22c61916c328ba44f1d2f5cbd6ad7f8a
KevinWissmer <kevin.wissmer@mail.de>
2023-08-10 19:37:48 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,123 @@
-import { Component } from '@angular/core';
+import { Firestore, onSnapshot, serverTimestamp, addDoc, collection, collectionData, doc, setDoc, deleteDoc, updateDoc, deleteField } from '@angular/fire/firestore';
+import { Component, inject } from '@angular/core';
+import { Observable } from 'rxjs';
 
+export interface ToDo {
+  name: string;
+  number?: number;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  currentIndex = 1;
   title = 'FirebaseProjektFull';
+
+  //todo interface liegt an anderer stelle
+  todos$: Observable<ToDo[]>;
+  unsub;
+  unsubSingle;
+  unsubChange;
+  todolist: ToDo[] = [];
+  todoCollection
+
+  //andere Option:
+  // private firestore: Firestore = inject(Firestore);
+
+  constructor(firestore: Firestore) {
+
+    //subscribe classic
+    this.todoCollection = collection(firestore, '/todos');
+    //this.todoCollection = collection(firestore, '/todos/2/subcoll');   auch möglich direkt via pfad die subcollection auszuwählen
+    this.todos$ = collectionData(this.todoCollection) as Observable<ToDo[]>;
+    this.todos$.subscribe((allTodos) => { this.todolist = allTodos })
+
+    //snapshot
+    //ganze Collecetion subscriben
+    this.unsub = onSnapshot(this.todoCollection, (allTodos) => {
+      allTodos.forEach((singleTodo) => {
+        console.log("Current data: ", singleTodo.data());
+      });
+    });
+
+    //einzelnes document mit der ID "2" subscriben
+    this.unsubSingle = onSnapshot(doc(this.todoCollection, '254sd6f5465ds4fg'), (singleTodo) => {
+      console.log("document: ", singleTodo.data());
+    });
+
+    //ganze Collecetion subscriben mit docChanges()
+    this.unsubChange = onSnapshot(this.todoCollection, (allTodos) => {
+      allTodos.docChanges().forEach((change) => {
+        if (change.type === "added") {
+          console.log("New Todo: ", change.doc.data());
+        }
+        if (change.type === "modified") {
+          console.log("Modified Todo: ", change.doc.data());
+        }
+        if (change.type === "removed") {
+          console.log("Removed Todo: ", change.doc.data());
+        }
+      })
+    });
+
+  }
+
+
+  ngOnDestroy() {
+    this.unsub()
+    this.unsubSingle();
+    this.unsubChange();
+  }
+
+
+  //document hinzufügen
+  async addList() {
+    let todoRef = await addDoc(this.todoCollection, {
+      name: "Dein Titel",
+    })
+    .catch(err => console.log(err))
+    .then((todoRef) => { console.log("Document written with addDoc: ", todoRef?.id) });
+    ;
+  }
+
+  // löschen eines ganzen dokuments
+  async deleteList(id: string) {
+    await deleteDoc(doc(this.todoCollection, id)).catch(err => console.log(err));
+  }
+
+  //löschen eines feldes
+  async deleteSingleField(id:string) {
+    await updateDoc(doc(this.todoCollection, id), {
+      number: deleteField()
+    }).catch(err => console.log(err));
+  }
+
+  
+
+  //feld hinzufügen oder updaten eines feldes
+  async addSingleField(id:string) {
+    await updateDoc(doc(this.todoCollection, id), {
+      number: "2"
+    }).catch(err => console.log(err));
+
+    //   ebenso möglich verschachtelung:
+    //   await updateDoc(frankDocRef, {
+    //     "age": 13,
+    //     "favorites.color": "Red"
+    //    });
+
+    //   Daten hätten folgendes Design:
+    //     {
+    //     name: "Frank",
+    //     favorites: { food: "Pizza", color: "Blue", subject: "recess" },
+    //     age: 12
+    //      }
+
+  }
+
+  logTimestamp() {
+    console.log(serverTimestamp());
+  }
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: '+++++++++++++',
+    appId: '1:+++++++++++++++:web:++++++++++++++++',
+    storageBucket: '+++++++++++++.appspot.com',
+    apiKey: '+++++++++++++++++++++++++++++++++',
+    authDomain: '++++++++.firebaseapp.com',
+    messagingSenderId: '++++++++',
+  },
+};

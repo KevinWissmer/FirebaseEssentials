edaacd03b9236bbb2fa0ac1cc18fb903bef0920c
KevinWissmer <kevin.wissmer@mail.de>
2023-08-10 20:21:32 +0200

    child deleted from appModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,12 +8,10 @@ import { environment } from '../environments/environment';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideDatabase,getDatabase } from '@angular/fire/database';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ChildComponent
   ],
   imports: [
     BrowserModule,
